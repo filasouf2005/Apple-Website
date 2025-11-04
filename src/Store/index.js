b22db773e0a7b911e4d6ce7f522f987516ab1def
filasouf2005 <nefsiabdelouahab@gmail.com>
2025-11-04 20:14:29 +0100

    Implement the Feateur Section

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,13 +1,17 @@
 import { create } from "zustand";
 
-const useMackbookStore = create((set) => ({
+const useMacbookStore = create((set) => ({
   color: "#2e2c2e",
   setColor: (color) => set({ color }),
 
   scale: 0.08,
   setScale: (scale) => set({ scale }),
 
-  reste: () => ({ color: "#2e2c3e", scale: 0.08 }),
+  texture: "/videos/feature-1.mp4",
+  setTexture: (texture) => set({ texture }),
+
+  reset: () =>
+    set({ color: "#2e2c2e", scale: 0.08, texture: "/videos/feature-1.mp4" }),
 }));
 
-export default useMackbookStore;
+export default useMacbookStore;
